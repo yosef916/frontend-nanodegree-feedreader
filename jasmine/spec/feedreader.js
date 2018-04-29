17d0735f7ef5b6657427de07fca553006ed253d6
yosef916 <yussef916@gmail.com>
2018-04-29 17:53:56 +0200

    some edits
    when the loadFeed function is called & completes its work.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -83,19 +83,20 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
-        it('when the loadFeed function is called & completes its work', function() {
           beforeEach(function(done) {
             loadFeed(0, function() {
               done();
             });
-          });
-        });
 
+          it('when the loadFeed function is called & completes its work', function() {
+            expect($('.feed .entry').length).not.toBe(0);
+            });
+          });
+    });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
-         */
-    });
+         */    
 }());
